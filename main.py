7f8889b0b90fd9b1e77234b526fc26682673a012
Jonwodi <>
2021-05-10 06:14:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+# Tuple items are enclosed in round brackets
+# Tuple is ordered
+# Tuple is immutable
+# Tuple elements do not need to be unique
+# Elements can be of different data type
+
+'''
+Creating a Tuple
+'''
+
+# tuple = ()
+# tuple = ("Jay", [1,2,3], 100, 2.50, 1 + 4j, (1,2,3))
+# tuple = "Jay",1234
+# tuple = "Jay",
+# tuple = ("Jay",)
+# print(type(tuple))
+# print(tuple)
+
+
+
+'''
+Access Tuple Elements
+'''
+# tuple = (1234, 4321, "Day")
+# print(tuple[0])
+# print(tuple[-1])
+
+
+'''
+Slicing Tuples in Python
+'''
+# Celebrities = ("Chris Brown", "Lebron James", "Kobe Brynat", "Dave East")
+# print(Celebrities[:])
+# print(Celebrities[2:4])
+# print(Celebrities[:-2])
+# print(Celebrities[: 2])
+# print(Celebrities[2:])
+# print(Celebrities[::2])
+# print(Celebrities[::-1])
+
+'''
+Changing a Tuple
+'''
+# Celebrities = ("Chris Brown", "Lebron James", "Kobe Brynat", "Dave East")
+# Celebrities[0] = ("J cole")
+# print(Celebrities)
+
+
+
+'''
+Deleting a Tuple
+'''
+# Celebrities = ("Chris Brown", "Lebron James", "Kobe Brynat", "Dave East")
+# del Celebrities[0]
+# print(Celebrities)
+
+# del Celebrities 
+# print(Celebrities)
+
+
+'''
+Tuple Methods
+'''
+
+# print(dir(tuple))
+
+# Celebrities = ("Chris Brown", "Lebron James", "Kobe Brynat", "Dave East", "Chris Brown")
+
+# print(Celebrities.index("Dave East"))
+# print(Celebrities.count("Chris Brown"))
